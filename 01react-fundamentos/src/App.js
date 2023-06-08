6e7f849b1889588cdb00f750676543bf12b057f6
Jason Balamb <jasonst.106@gmail.com>
2023-06-08 17:20:22 -0500

    editing App.js

--- 01react-fundamentos/src/App.js
+++ 01react-fundamentos/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import './App.css';
+
+const todos = [
+  { text: 'cut onions', completed: false },
+  { text: 'take the introductory course to React', completed: false },
+  { text: 'cry intensely', completed: false },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <React.Fragment>
+    {/* <TodoCounter /> */}
+    <h2>
+      You've completed 2 out of 3 Todo
+    </h2>
+
+    {/* <TodoSearch /> */}
+    <input placeholder="Onion" />
+
+    {/* <TodoList>
+      {todos.map(todo => (
+        <TodoItem />
+      ))}
+    </TodoList> */}
+
+    {/* <CreateTodoButton/> */}
+    <button>+</button>
+  </React.Fragment>
   );
 }
 
